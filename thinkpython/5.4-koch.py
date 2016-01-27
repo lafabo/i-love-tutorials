10c0652558df89f3d5a534ae928117ce344dee0b
lafabo <lazyfatboy@ya.ru>
2016-01-27 19:16:28 +0300

    day 50! hurray!

--- thinkpython/5.4-koch.py
+++ thinkpython/5.4-koch.py
@@ -18,9 +18,16 @@ def koch(turtle, x):
 	koch(turtle, x/3)
 
 
+def snowflake(turtle, lenth):
+	for i in range(3):
+		koch(turtle, lenth)
+		lt(turtle, 120)
+
 world = TurtleWorld()
 bob = Turtle()
 bob.delay = 0.001
-koch(bob, 1200)
+# koch(bob, 100)
+snowflake(bob, 100)
+
 
 wait_for_user()
---
+++ thinkpython/6.-factorial.py
@@ -0,0 +1,9 @@
+def factorial(n):
+	if n == 0:
+		return 1
+	else:
+		return n * factorial(n-1)
+
+print factorial(1)
+print factorial(20)
+print factorial(5)
