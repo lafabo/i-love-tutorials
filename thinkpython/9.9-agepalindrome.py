d64e3a3dbc5f70e995e1b8a131c9ab2f0e035ad3
lafabo <lazyfatboy@ya.ru>
2016-02-04 20:40:21 +0300

    day 58

--- thinkpython/9.9-agepalindrome.py
+++ thinkpython/9.9-agepalindrome.py
@@ -1,6 +1,16 @@
 momstart = 73-37
 sonstrat = 0
 
+'''
 for i in range(0, 99):
 	if str(momstart+i) == str(sonstrat+i)[::-1]:
 		print momstart+i, sonstrat+i
+'''
+
+
+
+for i in xrange(0, 99):
+	for j in xrange(0, 99):
+		if str(i) == str(j)[::-1]:
+			print i, j
+	print '\n'
