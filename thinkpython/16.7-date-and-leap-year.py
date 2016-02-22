364b25572b1b596dfd06739b5c7eb55f76758c60
lafabo <lazyfatboy@ya.ru>
2016-02-22 22:57:16 +0300

    day 77

---
+++ thinkpython/16.7-date-and-leap-year.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class Date(object):
+	"""
+	day month year
+	"""
+
+
+def increment_date(date, ndays):
+	jan = mar = may = jul = aug = okt = dec = 31
+	1 = 3 = 5 = 7 = 8 = 10 = 12
+	apr = jun = sen = nov = 30
+	4 = 6 = 9 = 11 = 30
+
+	feb
+
+
+
+
+if __name__ == '__main__':
+	d = Date()
+	d.d = 1
+	d.m = 12
+	d.y = 2015
+
