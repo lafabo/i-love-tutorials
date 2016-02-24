99459ff75c4eef1a0dee1848cc7bc6ea4a616c75
lafabo <lazyfatboy@ya.ru>
2016-02-24 22:19:13 +0300

    day 78

--- thinkpython/16.7-date-and-leap-year.py
+++ thinkpython/16.7-date-and-leap-year.py
@@ -9,12 +9,19 @@ class Date(object):
 
 
 def increment_date(date, ndays):
-	jan = mar = may = jul = aug = okt = dec = 31
-	1 = 3 = 5 = 7 = 8 = 10 = 12
-	apr = jun = sen = nov = 30
-	4 = 6 = 9 = 11 = 30
 
-	feb
+	# set up if month is february
+	if year % 4 == 0:
+		avalable_days = 28
+	else:
+		avalable_days = 29
+
+	if date.m in (1, 3, 5, 7, 8, 10, 12):
+		avalable_days = 31
+	elif date.m in (4, 9, 11):
+		avalable_days = 30
+
+
 
 def is_year_leap(date):
 	pass
