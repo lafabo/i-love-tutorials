169e65431d0e43a7e0d7608337518094cbab2b2a
lafabo <lazyfatboy@ya.ru>
2016-03-20 23:19:18 +0300

    day 103

---
+++ lutz_study/2-stan/txtreadlines.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def more(text, numlines=15):
+	lines = text.splitlines()
+	while lines:
+		chunk = lines[:numlines]
+		lines = lines[numlines:]
+		for line in chunk:
+			print(line)
+		if lines and input('More? [y]: ') not in ['y', 'Y']:
+			break
+
+
+if __name__ == '__main__':
+	import sys
+	more(open(sys.argv[1]).read(), 10)
