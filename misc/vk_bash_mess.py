5221767cc3e921e74cd8e3f69593de7fec19a63c
lafabo <lazyfatboy@ya.ru>
2016-03-26 10:19:57 +0300

    day 108

---
+++ misc/vk_bash_mess.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+import json
+import urllib2
+import vk_auth
+
+app_id = '4925055'
+login = input(u"Enter your login: ")
+password = input(u"Enter your pass: ")
+
+
+class Connect
+
+if __name__ == '__main__':
+	os.system('clear')
