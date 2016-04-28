3c1bd8ae7979c73ea2d5881fadbbb32aa1e139b8
lafabo <lazyfatboy@ya.ru>
2016-04-28 23:15:08 +0400

    day 142

---
+++ misc/tinder-new-people-notification.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pynder
+import time
+
+fb_id = ''
+fb_auth_token = ''
+
+
+def look_for_new_users():
+	session = pynder.Session(fb_id, fb_auth_token)
+	users = session.nearby_users()
+	return users
+
+
+def timer():
+	while True:
+		time.sleep(60)
+
+
+notify = False
+
+
+def notify(users):
+	if users and len(users) > 0:
+		return True
+	return False
+
+
+if __name__ == '__main__':
+	pass
