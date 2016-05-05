a0c66df902509861e365882aaa001a3098b86b19
lafabo <lazyfatboy@ya.ru>
2016-05-05 22:04:36 +0400

    day 149

--- misc/vk_books_flibusta.py
+++ misc/vk_books_flibusta.py
@@ -2,6 +2,10 @@
 # -*- coding: utf-8 -*-
 from os.path import isfile
 import vk_api
+import time
+import zipfile
+import re
+
 
 def check_source(argv):
 	# vk post
@@ -17,7 +21,8 @@ def check_source(argv):
 def parse_books_from_list(booklist):
 	# here would be re for author - "book", 'book' - author, and etc
 	# return all text
-	pass
+	authors_books = re.findall(r'?:[A-ZА-Я](?:\.|[a-zа-я\-]+)\s?)+)[\.\s-]*["«']([^"«'»]+)["'«»]', booklist)
+	return authors_books
 
 
 def parse_from_vk(link):
@@ -48,8 +53,25 @@ def download_book(author_book):
 	pass
 
 
+def check_result(list_in, list_out):
+	unsuccessful = list(set(list_in) - set(list_out))
+	successful = list(set(list_in) - set(unsuccessful))
+	# print results:
+	print('Successful downloaded:\n')
+	for book in successful:
+		print book
+	print('Not found:\n')
+	for book in unsuccessful:
+		print book
+	return successful, unsuccessful
+
+
 def downloaded_to_zip(files):
-	pass
+	filename = str(time.time()) + '.zip'
+	with zipfile.ZipFile(filename, 'w') as f:
+		for book in files:
+			f.write(book)
+	print('\nSaved as %s' % filename)
 
 
 if __name__ == '__main__':
