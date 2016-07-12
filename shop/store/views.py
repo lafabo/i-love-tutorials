c47f0a98dcf4b1c62b936d6b7865d6682ceea09b
lafabo <lazyfatboy@ya.ru>
2016-07-12 16:26:22 +0500

    day 10

---
+++ shop/store/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import *
+
+
+# Create your views here.
+def index(request):
+    template = loader.get_template('index.html')
+    context = {
+        'categories': Category.objects.all(),
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def product(request, id):
+    template = loader.get_template('product.html')
+    context = {
+        'product': Product.objects.get(id=id),
+    }
+    return HttpResponse(template.render(context, request))
